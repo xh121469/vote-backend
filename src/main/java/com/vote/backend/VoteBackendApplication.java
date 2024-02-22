71ef44dfa32628d17e883ba2ed1edd4e13913d74
xh <8799580+woniuxuhao@user.noreply.gitee.com>
2024-02-22 14:35:48 +0800

    2024.2.22  初始框架

---
+++ src/main/java/com/vote/backend/VoteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vote.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoteBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vote/backend/VoteBackendApplicationTests.java
@@ -0,0 +1,14 @@
+package com.vote.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("测试");
+    }
+
+}
